50a899f5525a6969f7e89f470a154c10c1013182
Marcel Teruo Nakamine <marcel.nakamine@gmail.com>
2016-03-01 03:41:48 -0300

    configure the automation (gulp watch)

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,70 @@ var inject = require('gulp-inject');
 var paths = {
   temp: 'temp',
   tempVendor: 'temp/vendor',
-  index: 'app/index.html'
+  tempIndex: 'temp/index.html',
+
+  index: 'app/index.html',
+  appSrc: ['app/**/*', '!app/index.html'],
+  bowerSrc: 'bower_components/**/*'
 };
 
-gulp.task('default', ['scripts','serve']);
+gulp.task('default', ['watch']);
 
-gulp.task('scripts',function() {
-  var tempIndex = gulp.src(paths.index).pipe(gulp.dest(paths.temp));
+gulp.task('watch', ['serve'], function() {
+  gulp.watch(paths.appSrc, ['scripts']);
+  gulp.watch(paths.bowerSrc, ['vendors']);
+  gulp.watch(paths.index, ['copyAll']);
+});
 
-  var scripts = gulp.src('app/**/*.js').pipe(gulp.dest(paths.temp));
+gulp.task('serve', ['copyAll'], function() {
+  return gulp.src(paths.temp)
+  .pipe(webserver({
+    livereload: true,
+    proxies: [{
+      source: '/api',
+      target: 'http://localhost:1337'
+    }]
+  }));
+});
 
+gulp.task('copyAll', function() {
   var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
 
-  tempIndex.pipe(inject(scripts, {
-    relative: true}))
+  var appFiles = gulp.src(paths.appSrc).pipe(gulp.dest(paths.temp));
+
+  return gulp.src(paths.index)
+  .pipe(gulp.dest(paths.temp))
   .pipe(inject(tempVendors, {
-    relative: true, name: 'vendorInject'}))
+    relative: true,
+    name: 'vendorInject'
+  }))
+  .pipe(inject(appFiles, {
+    relative: true
+  }))
   .pipe(gulp.dest(paths.temp));
 });
 
-gulp.task('serve', function() {
-  gulp.src(paths.temp)
-  .pipe(webserver({
-    open: true
-  }));
+gulp.task('vendors', function() {
+  var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
+
+  return gulp.src(paths.tempIndex)
+  .pipe(inject(tempVendors, {
+    relative: true,
+    name: 'vendorInject'
+  }))
+  .pipe(gulp.dest(paths.temp));
+});
+
+gulp.task('scripts',function() {
+  var appFiles = gulp.src(paths.appSrc).pipe(gulp.dest(paths.temp));
+
+  return gulp.src(paths.tempIndex)
+  .pipe(inject(appFiles, {
+    relative: true
+  }))
+  .pipe(gulp.dest(paths.temp));
+});
+
+gulp.task('clean', function() {
+  del([paths.temp]);
 });
