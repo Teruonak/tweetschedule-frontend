118d2671df74b078e5c95a43d6ad0a6f08555d94
Marcel Teruo Nakamine <marcel.nakamine@gmail.com>
2016-03-01 02:03:54 -0300

    config gulp for deploy in temp folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
 gulp.task('default', function() {
-  gulp.src('root')
+  gulp.src('app/index.html')
+  .pipe(gulp.dest('temp'));
+
+  gulp.src('temp')
   .pipe(webserver({
-    open: true,
+    open: true
   }));
 });
