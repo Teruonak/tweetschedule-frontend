dfda73986217930c8c23e39d1c7ace10a454ada5
Marcel Teruo Nakamine <marcel.nakamine@gmail.com>
2016-03-01 02:09:39 -0300

    improve the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
-gulp.task('default', function() {
-  gulp.src('app/index.html')
-  .pipe(gulp.dest('temp'));
+var paths = {
+  temp: 'temp',
+  index: 'app/index.html'
+};
 
-  gulp.src('temp')
+gulp.task('default', ['tempSetup','serve']);
+
+gulp.task('tempSetup', function() {
+  gulp.src(paths.index)
+  .pipe(gulp.dest(paths.temp));
+});
+
+gulp.task('serve', function() {
+  gulp.src(paths.temp)
   .pipe(webserver({
     open: true
   }));
